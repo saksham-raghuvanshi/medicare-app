8a37f38d6510eeeeaaa02fc2979880866f5ffa5f
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-08-15 23:39:25 +0530

    Hero section header and para

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -1,5 +1,159 @@
+import logo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import { RiLinkedinFill } from "react-icons/ri";
+import {
+  AiFillYoutube,
+  AiFillGithub,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+
+const sociallinks = [
+  {
+    path: "/",
+    icon: <AiFillGithub className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "/",
+    icon: <AiFillYoutube className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "/",
+    icon: <AiOutlineInstagram className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "/",
+    icon: <RiLinkedinFill className="group-hover:text-white w-4 h-5" />,
+  },
+];
+
+const quickLinks01 = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/about-us",
+    display: "About-us",
+  },
+  {
+    path: "/blog",
+    display: "Blog",
+  },
+];
+
+const quickLinks02 = [
+  {
+    path: "/find-a-doctor",
+    display: "Find a Doctor",
+  },
+  {
+    path: "/",
+    display: "Request an Appointment",
+  },
+  {
+    path: "/",
+    display: "Find a Location",
+  },
+  {
+    path: "/",
+    display: "Get a Opinion",
+  },
+];
+
+const quickLinks03 = [
+  {
+    path: "/",
+    display: "Donate",
+  },
+  {
+    path: "/contact",
+    display: "Contact-us",
+  },
+];
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="pb-16 pt-10">
+      <div className="container">
+        <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
+          <div>
+            <img src={logo} alt="medicare-logo" />
+            <p className="text-[16px] leading-7 font-[400]">
+              Copyright ©️ {year} developed by Saksham Raghuvanshi All right
+              reserved
+            </p>
+            <div className="flex items-center gap-3 mt-4">
+              {sociallinks.map((link, index) => (
+                <Link
+                  to="link.path"
+                  key={index}
+                  className="w-9 h-9 rounded-full flex items-center border border-solid border-[#181A1E] justify-center group hover:bg-primaryColor hover:border-none "
+                >
+                  {link.icon}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              Quick Links
+            </h2>
+            <ul>
+              {quickLinks01.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              I want to:
+            </h2>
+            <ul>
+              {quickLinks02.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              Support
+            </h2>
+            <ul>
+              {quickLinks03.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,5 +1,30 @@
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <>
+      <>
+        <section className="hero__section pt-[60px] 2xl:h-[800px]">
+          <div className="container">
+            <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+              <div>
+                <div className="lg:w-[570px]">
+                  <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">
+                    We help patients live a healthy, longer life
+                  </h1>
+                  <p className="text_para">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Integer non accumsan sem. Aenean imperdiet erat eu nisi
+                    ultricies facilisis. Etiam ut velit dolor. Quisque lobortis
+                    nisl a vestibulum aliquet.
+                  </p>
+                  <button className="btn">Request an Appointment</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    </>
+  );
 };
 
 export default Home;

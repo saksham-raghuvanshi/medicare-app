0680ec58a2aae173fff0e77bbefbe8ed61b61d8b
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-08-17 16:38:20 +0530

    Services map

--- src/Services/ServicesList.jsx
+++ src/Services/ServicesList.jsx
@@ -1,5 +1,8 @@
+import { services } from "../../data/services.js";
 const ServicesList = () => {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] - lg:mt-[55px]"></div>
+  );
 };
 
 export default ServicesList;

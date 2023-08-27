c2ef44a2cf59c206a22ea4017b1eb808c15dc36c
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-08-27 15:23:26 +0530

    Scroll to Top

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTops from "./Others/ScrollToTops.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
+      <ScrollToTops />
       <App />
     </Router>
   </React.StrictMode>

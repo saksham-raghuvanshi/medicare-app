1ef6f89bdd21e5719cd139250385a9342bf3c250
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-08-28 13:45:49 +0530

    Login Page created

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,63 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  return (
+    <section className="px-5 lg:px-0">
+      <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md p-5 md:p-10">
+        <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
+          Hello! <span className="text-primaryColor">Welcome</span> Back 🎉
+        </h3>
+        <form className="py-4 md:py-0">
+          <div className="mb-5">
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className="w-full  py-4 border-b border-solid  border-[#006ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              className="w-full py-4 border-b border-solid  border-[#006ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+              required
+            />
+          </div>
+          <div className="mt-7">
+            <button
+              type="submit"
+              className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg py-3"
+            >
+              Login
+            </button>
+          </div>
+
+          <p className="mt-5 text-textColor text-center">
+            Don&apos;t have and account{" "}
+            <Link to="/register" className="text-primaryColor font-medium ">
+              Register
+            </Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
